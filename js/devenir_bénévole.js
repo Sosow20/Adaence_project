97d769573f335c2e9b8721be862d4148aab4d4cc
Sosow20 <sofiacherifi1995@gmail.com>
2025-09-11 00:57:30 +0200

    update: html & css

---
+++ js/devenir_bénévole.js
@@ -0,0 +1,10 @@
+
+/*POUR LE FORMULAIRE:
+
+Dans le cadre de Vanilla JS et la gestion des formulaires,
+pour éviter le rafraîchissement de la page au clique d'un bouton, n'oubliez pas d'ajouter e.preventDefault();.
+document.querySelector('#search-button').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  console.log('hellooooo');
+});*/
---
+++ js/je_rends_visite.js
@@ -0,0 +1,34 @@
+
+const getAdaenceData = async () => {
+    const res = await fetch("../data/Adaence.json")
+    const data = await res.json()
+    return data
+}
+
+
+const profils = document.querySelectorAll('.display-data');
+
+const displayProfils = async () => {
+    const dataAda = await getAdaenceData();
+
+    profils.forEach((div, index) => {
+        const user = dataAda[index];
+        if (!user) return;
+
+        div.innerHTML = `
+        <div class="profil-text">
+            <ul>
+                <li> <strong> Type de moment:</strong> ${user.type} </li>
+                <li> <strong>Prénom:</strong>  ${user.firstname} </li>
+                <li> <strong>Métier:</strong> ${user.job} • ${user.age} ans </li>
+                <li> <strong>Localisation:</strong> ${user.city} </li>
+                <li> <strong>Description:</strong> ${user.description} </li>
+            </ul>
+      </div>
+    `;
+    });
+};
+
+// filter includes addEventLisener => bouton recherche gitcopilot 
+
+displayProfils();
